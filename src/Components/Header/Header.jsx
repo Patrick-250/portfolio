1b2e328c87b7d82d0997a51c47adda0ce3422cfb
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-04-15 17:50:46 -0500

    add menu bar for smaller screens

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/profilelogo.png";
 
 import { FaLinkedinIn } from "react-icons/fa6";
 import { HiOutlineMail } from "react-icons/hi";
 import { FaTwitter } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import "./Header.css";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="profile-container">
@@ -16,7 +23,10 @@ const Header = () => {
           <img className="profile-logo" src={logo} alt="logo"></img>
         </a>
       </div>
-      <nav className="nav-links">
+      <div className="menu-icon" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <nav className={`nav-links ${menuOpen ? "active" : ""}`}>
         <Link to="/about">About</Link>
         <Link to="/skills">Skills</Link>
         <Link to="/experience">Experience</Link>
